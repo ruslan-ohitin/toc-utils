2eb8e1b5bf905c23d5f31ec8f77b115921183bef
Ruslan Ohitin <ruslan.ohitin@gmail.com>
2022-09-01 15:26:06 +0700

    Add dpi as parameter

--- toc_ocr.py
+++ toc_ocr.py
@@ -3,11 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pdf_file', help='PDF file name')
-parser.add_argument('start_page', help='Page to start from', type=int)
+parser.add_argument('start_page', help='Page number to start from. Don\'t mix it with page label!', type=int)
 parser.add_argument('page_count', help='Number of pages to process', type=int, default=1)
 parser.add_argument('-l', '--lang', 
                     help='Language files for tesseract. Use + sign to specify myltiple files: rus+eng',
                     default='rus')
+parser.add_argument('-d', '--dpi', help='Resolution in dpi to render PDF page', type=int, default=300)
 
 args = parser.parse_args()
 
@@ -15,8 +16,9 @@ doc = fitz.open(args.pdf_file)
 
 page_text = ''
 for page_index in range(args.page_count):
-    page = doc[args.start_page + page_index - 1] 
-    full_tp = page.get_textpage_ocr(flags=0, dpi=300, full=True, language=args.lang)
+    # Pages starts from 0, we expext page number 1 based in user input 
+    page = doc[args.start_page - 1 + page_index] 
+    full_tp = page.get_textpage_ocr(flags=0, dpi=args.dpi, full=True, language=args.lang)
     page_text = page_text + page.get_text(textpage=full_tp)
 
 print(page_text)
