992e9c1680b49365272c81dc7d6977b3f4db9755
Ruslan Ohitin <ruslan.ohitin@gmail.com>
2022-09-01 15:39:48 +0700

    Write to file with UTF-8 encoding. Add Makefile to copy scripts to path

---
+++ Makefile
@@ -0,0 +1,3 @@
+c:\bin\toc_ocr.py: toc_ocr.py
+	copy toc_ocr.py c:\bin\
+
--- toc_ocr.py
+++ toc_ocr.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('pdf_file', help='PDF file name')
 parser.add_argument('start_page', help='Page number to start from. Don\'t mix it with page label!', type=int)
 parser.add_argument('page_count', help='Number of pages to process', type=int, default=1)
+parser.add_argument('-o', '--out', help='Output file name', default='index.txt')
 parser.add_argument('-l', '--lang', 
                     help='Language files for tesseract. Use + sign to specify myltiple files: rus+eng',
                     default='rus')
@@ -14,12 +15,13 @@ args = parser.parse_args()
 
 doc = fitz.open(args.pdf_file)
 
-page_text = ''
+page_text = '' # Var to collect text from ocr
 for page_index in range(args.page_count):
     # Pages starts from 0, we expext page number 1 based in user input 
     page = doc[args.start_page - 1 + page_index] 
     full_tp = page.get_textpage_ocr(flags=0, dpi=args.dpi, full=True, language=args.lang)
     page_text = page_text + page.get_text(textpage=full_tp)
 
-print(page_text)
-
+toc_file = open(args.out, "w", encoding="utf8")
+toc_lines = toc_file.write(page_text)
+toc_file.close()
