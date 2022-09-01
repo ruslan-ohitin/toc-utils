9c264ea48496ae68006eba5974ed5af5fbf8f591
Ruslan Ohitin <ruslan.ohitin@gmail.com>
2022-09-01 15:58:50 +0700

    Add CLI params to toc_set.py

--- toc_set.py
+++ toc_set.py
@@ -1,8 +1,3 @@
-# pymupdf inport
-import fitz
-
-doc = fitz.open("1.pdf")
-
 # Page Labels
 # https://pymupdf.readthedocs.io/en/latest/document.html#Document.set_page_labels
 # print(doc.get_page_labels())
@@ -10,8 +5,21 @@ doc = fitz.open("1.pdf")
 # TOC Entries
 # https://pymupdf.readthedocs.io/en/latest/document.html#Document.set_toc
 
+import fitz # pymupdf inport
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('pdf_file', help='PDF file name')
+parser.add_argument('-i', '--index', help='File name with TOC index', default='index.txt')
+parser.add_argument('-o', '--out', help='Modified PDF file name', default='out.pdf')
+parser.add_argument('-d', '--delta', help='Number to add to (or subtract from) page number in TOC index file', type=int default=0)
+
+args = parser.parse_args()
+
+doc = fitz.open(args.pdf_file)
+
 # Read plain text file with TOC entries to toc_lines.
-toc_file = open("index.txt", "r", encoding="utf8")
+toc_file = open(args.index, "r", encoding="utf8")
 toc_lines = toc_file.readlines()
 toc_file.close()
 
@@ -21,9 +29,11 @@ for line in toc_lines:
     tokens = line.split("#")
     level = int(tokens[0])
     title = tokens[1].strip()
-    page = int(tokens[2])
+    page = int(tokens[2]) + args.delta
 
     toc.append([level, title, page])
 
 doc.set_toc(toc, collapse=0)
-doc.save("2.pdf")
+if args.out:
+    doc.save(args.out)
+
